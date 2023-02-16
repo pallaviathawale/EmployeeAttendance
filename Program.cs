42e3f6dab4c37adcc32b66b0205e4d77856f8c77
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-16 09:44:38 +0530

    Add: Program.cs file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeAttendance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if(empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent ");
+
+            }
+        }
+    }
+}
